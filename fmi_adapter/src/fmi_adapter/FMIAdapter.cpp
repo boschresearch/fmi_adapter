f85817cfcce3979256d4884914f4edd24417fdda
Ralph Lange <ralph.lange@de.bosch.com>
2020-01-28 08:59:58 +0100

    Fixed sporadic exception in case of small external steps.
    Signed-off-by: Ralph Lange <ralph.lange@de.bosch.com>

--- fmi_adapter/src/fmi_adapter/FMIAdapter.cpp
+++ fmi_adapter/src/fmi_adapter/FMIAdapter.cpp
@@ -438,7 +438,7 @@ rclcpp::Time FMIAdapter::doStepsUntil(const rclcpp::Time & simulationTime)
   }
 
   fmi2_real_t targetFMUTime = (simulationTime - fmuTimeOffset_).seconds();
-  if (targetFMUTime < fmuTime_) {
+  if (targetFMUTime < fmuTime_ - stepSize_.seconds() / 2.0) {  // Subtract stepSize/2 for rounding.
     RCLCPP_ERROR(logger_, "Given time %f is before current simulation time %f!", targetFMUTime,
       fmuTime_);
     throw std::invalid_argument("Given time is before current simulation time!");
